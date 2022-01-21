85844b97f363b499fa0142f4dbcdbbc4363c4cc4
Antanas Burneika <an.burneika@gmail.com>
2022-01-21 14:31:00 +0200

    hidden top header

--- App.tsx
+++ App.tsx
@@ -6,16 +6,13 @@ import ProfileScreen from './screens/ProfileScreen'
 
 import AppContext from './components/AppContext';
 
-
-
-
 const Stack = createNativeStackNavigator();
 
-
 export default function App() {
+
   const [loggedIn, setLoggedIn] = useState(false);
   const [token, setToken] = useState('');
-;
+  
 
   const appData = {
     loggedIn,
@@ -24,7 +21,6 @@ export default function App() {
     setToken,
   }
 
-
   return (
     <NavigationContainer>
       <AppContext.Provider value={appData}>
@@ -32,11 +28,11 @@ export default function App() {
             {
               !loggedIn ? (
                 <>
-                  <Stack.Screen name="Login" component={LoginScreen} />
+                  <Stack.Screen options={{headerShown: false}} name="Login" component={LoginScreen} />
                 </>
               ) : (
                 <>
-                  <Stack.Screen name="Profile" component={ProfileScreen} />
+                  <Stack.Screen options={{headerShown: false}} name="Profile" component={ProfileScreen} />
                 </>
               )
             }
